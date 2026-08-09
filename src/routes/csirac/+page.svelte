8ceb397a14bd5b8fe7e00505b89718ce79252144
LogolicusZ <adjordbra@gmail.com>
2026-08-09 15:45:28 +0200

    [c] updated images to avif

--- src/routes/csirac/+page.svelte
+++ src/routes/csirac/+page.svelte
@@ -3,29 +3,29 @@
   import Gallery from '$lib/components/Gallery.svelte';
 
   const hero = {
-    src: '/assets/img/csirac/render8.jpg',
+    src: '/assets/img/csirac/render8.avif',
     alt: 'CSIRAC render 8',
     width: 2872,
     height: 1452
   };
 
   const images = [
-    { src: '/assets/img/csirac/render1.jpg', alt: 'Render 1', width: 2353, height: 1324 },
-    { src: '/assets/img/csirac/render2.jpg', alt: 'Render 2', width: 2353, height: 1324 },
-    { src: '/assets/img/csirac/render3.jpg', alt: 'Render 3', width: 2353, height: 1324 },
-    { src: '/assets/img/csirac/render4.jpg', alt: 'Render 4', width: 2353, height: 1324 },
-    { src: '/assets/img/csirac/render5.jpg', alt: 'Render 5', width: 2353, height: 1324 },
-    { src: '/assets/img/csirac/render6.jpg', alt: 'Render 6', width: 2353, height: 1324 },
-    { src: '/assets/img/csirac/render7.jpg', alt: 'Render 7', width: 2353, height: 1324 },
-    { src: '/assets/img/csirac/render9.jpg', alt: 'Render 9', width: 3043, height: 1712 },
-    { src: '/assets/img/csirac/screenshot1.png', alt: 'Screenshot 1', width: 1830, height: 726 },
-    { src: '/assets/img/csirac/screenshot2.png', alt: 'Screenshot 2', width: 948, height: 397 },
-    { src: '/assets/img/csirac/screenshot3.png', alt: 'Screenshot 3', width: 1947, height: 804 },
-    { src: '/assets/img/csirac/screenshot4.png', alt: 'Screenshot 4', width: 1855, height: 754 },
-    { src: '/assets/img/csirac/screenshot5.png', alt: 'Screenshot 5', width: 1928, height: 779 },
-    { src: '/assets/img/csirac/screenshot6.png', alt: 'Screenshot 6', width: 1884, height: 772 },
-    { src: '/assets/img/csirac/screenshot7.png', alt: 'Screenshot 7', width: 1770, height: 711 },
-    { src: '/assets/img/csirac/screenshot8.png', alt: 'Screenshot 8', width: 1837, height: 733 }
+    { src: '/assets/img/csirac/render1.avif', alt: 'Render 1', width: 2353, height: 1324 },
+    { src: '/assets/img/csirac/render2.avif', alt: 'Render 2', width: 2353, height: 1324 },
+    { src: '/assets/img/csirac/render3.avif', alt: 'Render 3', width: 2353, height: 1324 },
+    { src: '/assets/img/csirac/render4.avif', alt: 'Render 4', width: 2353, height: 1324 },
+    { src: '/assets/img/csirac/render5.avif', alt: 'Render 5', width: 2353, height: 1324 },
+    { src: '/assets/img/csirac/render6.avif', alt: 'Render 6', width: 2353, height: 1324 },
+    { src: '/assets/img/csirac/render7.avif', alt: 'Render 7', width: 2353, height: 1324 },
+    { src: '/assets/img/csirac/render9.avif', alt: 'Render 9', width: 3043, height: 1712 },
+    { src: '/assets/img/csirac/screenshot1.avif', alt: 'Screenshot 1', width: 1830, height: 726 },
+    { src: '/assets/img/csirac/screenshot2.avif', alt: 'Screenshot 2', width: 948, height: 397 },
+    { src: '/assets/img/csirac/screenshot3.avif', alt: 'Screenshot 3', width: 1947, height: 804 },
+    { src: '/assets/img/csirac/screenshot4.avif', alt: 'Screenshot 4', width: 1855, height: 754 },
+    { src: '/assets/img/csirac/screenshot5.avif', alt: 'Screenshot 5', width: 1928, height: 779 },
+    { src: '/assets/img/csirac/screenshot6.avif', alt: 'Screenshot 6', width: 1884, height: 772 },
+    { src: '/assets/img/csirac/screenshot7.avif', alt: 'Screenshot 7', width: 1770, height: 711 },
+    { src: '/assets/img/csirac/screenshot8.avif', alt: 'Screenshot 8', width: 1837, height: 733 }
   ];
 </script>
 
